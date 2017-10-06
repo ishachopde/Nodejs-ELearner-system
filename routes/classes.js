46bb8ff6e9948e9cce28e4ff5bf011a4387e47c7
isha chopde <isha.chopde250794@gmail.com>
2017-10-06 13:10:32 -0700

    new files

--- routes/classes.js
+++ routes/classes.js
@@ -3,12 +3,24 @@ var router = express.Router();
 
 var Class = require('../models/class');
 
-/* GET home page. */
+
+/* GET classes page */
 router.get('/', function(req, res, next) {
 	Class.getClasses(function(err,classes) {
 		// body...
+		if(err) throw err;
 		res.render('classes/index', { classes : classes });
 	},3);
+  
+});
+
+//get class details
+router.get('/:id/details', function(req, res, next) {
+	Class.getClassesById([req.params.id], function(err,classname) {
+		// body...
+		if(err) throw err;
+		res.render('classes/details', { class : classname });
+	});
   
 });
 
